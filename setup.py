b582fccb46e06f500d8c6500aa5a72ffad581960
Niranjan Bommu <niranjan.bommu@gmail.com>
2018-12-05 17:23:07 -0500

    adding setup file

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import akcli
+from setuptools import setup
+
+setup(name='akcli',
+    version=akcli.__version__,
+    description='Akamai CLI tool written in Python',
+    author="Niranjan Bommu",
+    author_email='niranjan.bommu@gmail.com',
+    url='https://github.com/nbommu/akcli-python',
+    packages=['akcli',],
+    scripts=['bin/akcli'],
+    keywords = ['akamai', 'dns', 'cli', 'client'],
+    install_requires=[
+                      'requests>=2.0',
+                      'edgegrid-python>=1.0.9',
+                      'click>=5.1',
+    ],
+)
